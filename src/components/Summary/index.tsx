804b3df610fde836f6ef2e59fcc5b7c21a9d9a6c
yago <yagosonicmassaroni@gmail.com>
2024-05-18 17:13:56 -0300

    :lipstick: estruturando e estilizando o projeto

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,15 @@
+import { SummaryContainer } from "./styles";
+
+export function Summary(){
+  return (
+    <SummaryContainer>
+        <div>
+          <header>
+            <span>Entradas</span>
+            
+          </header>
+        </div>
+    </SummaryContainer>
+
+  )
+}
